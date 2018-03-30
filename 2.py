a3960e6afc96b34aad4f92e1c5b2aaa9ba496738
legolas <814382690@qq.com>
2018-03-30 15:51:03 +0800

    添加逗号，匿名函数，返回函数

---
+++ 2.py
@@ -0,0 +1,19 @@
+# -*-coding:utf-8 -*-
+a = '1123456789'
+print a[1:2]
+print a[:-5]
+l = len(a)
+prefix = a[0:l%3]
+li = []
+for i in range(l/3,0,-1):
+    if i == 1:
+        b = -3
+        c = None
+    else:
+        b = -3 * i
+        c = int(b)+3
+    str1 = ','+a[b:c]
+    li.append(str1)
+str2 = "".join(li)
+print prefix+str2
+
