9051ae5ec3b8164c99375637d18578aad1d85c82
legolas <814382690@qq.com>
2018-03-30 15:51:30 +0800

    返回函数垒增

---
+++ 3.py
@@ -0,0 +1,16 @@
+# -*-coding:utf-8 -*-
+def comma(s):
+    n = len(s)
+    if n <=3:
+        return s
+    return comma(s[:n-3])
+
+# print comma('1245')
+
+def sum(s):
+    a = int(s)
+    a = a-1
+    if a<=0:
+        return a
+    return sum(a)+a
+print sum('12')
