a3337b4b946bb7e209ab49bf81a305aa2abba9ae
legolas-zeng <814382690@qq.com>
2018-05-23 23:33:00 +0800

    xlwt模块最多处理256个格子。不合要求。

--- 图片处理/excel.py
+++ 图片处理/excel.py
@@ -6,7 +6,6 @@ book = xlwt.Workbook()
 xlwt.add_palette_colour("custom_colour", 0x21)
 book.set_colour_RGB(0x21, 251, 228, 228)
 
-
 sheet1 = book.add_sheet('Sheet 1')
 style = xlwt.easyxf('pattern: pattern solid, fore_colour custom_colour')
 sheet1.write(0,0,'',style)
--- 图片处理/images.py
+++ 图片处理/images.py
@@ -1,16 +1,25 @@
 # coding=utf-8
 from PIL import Image
+import xlwt
 
-# 打开要处理的图像
-img_src = Image.open('C:\Users\Administrator\Desktop\hsq.jpg')
+book = xlwt.Workbook()
+xlwt.add_palette_colour("custom_colour", 0x21)
 
-# 转换图片的模式为RGBA
-img_src = img_src.convert('RGBA')
+im = Image.open("C:\Users\Administrator\Desktop\hsq.png")
+sheet1 = book.add_sheet('Sheet 1')
+pix = im.load()
+width = im.size[0]
+height = im.size[1]
 
-# 获得文字图片的每个像素点
-src_strlist = img_src.load()
+for x in range(width):
+    for y in range(height):
+        print x,y
+        colour = pix[x,y][0:3]
+        R = colour[0]
+        G = colour[1]
+        B = colour[2]
+        book.set_colour_RGB(0x21, R, G, B)
+        style = xlwt.easyxf('pattern: pattern solid, fore_colour custom_colour')
+        sheet1.write(x, y, '', style)
 
-# 100,100 是像素点的坐标
-data = src_strlist[100, 100]
-
-print data
+book.save('test.xls')
