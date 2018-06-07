5e1665e0721c42ca2eb11a795c132bae1da01d0f
legolas-zeng <814382690@qq.com>
2018-06-07 11:19:08 +0800

    添加zabbix-agent脚本

---
+++ 一键安装脚本/zabbix_agent.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+hostname=`hostname`
+########### 安装zabbix ##############
+yum install -y wget
+wget http://downloads.sourceforge.net/project/zabbix/ZABBIX%20Latest%20Stable/3.4.4/zabbix-3.4.4.tar.gz
+tar -zxvf zabbix-3.4.4.tar.gz
+cd zabbix-3.4.4
+./configure --enable-agent --prefix=/usr/local/zabbix_agentd
+make && make install
+############ 配置zabbix ###############
+
+mv /usr/local/zabbix_agentd/etc/zabbix_agentd.conf /usr/local/zabbix_agentd/etc/zabbix_agentd.conf.back
+cat > /usr/local/zabbix_agentd/etc/zabbix_agentd.conf << EOF
+local0.* /var/log/keepalived.log
+Server= 192.168.3.111,127.0.0.1
+ServerActive= 192.168.3.111
+Hostname = $hostname
+LogFile=/var/log/zabbix/zabbix_agentd.log
+PidFile=/var/run/zabbix/zabbix_agentd.pid
+Include=/usr/local/zabbix_agentd/etc/zabbix_agentd.d/*.conf
+EOF
+
+############## 添加防火墙 #############
+firewall-cmd --permanent --add-port=10050/tcp
+firewall-cmd --reload
+############# 设置开机自启动 #########
+sed -i '/usr/local/zabbix_agentd/sbin/zabbix_agentd -c /usr/local/zabbix_agentd/etc/zabbix_agent.conf' /etc/rc.local
+
+############# 启动zabbix服务 #########
+/usr/local/zabbix_agentd/sbin/zabbix_agentd -c /usr/local/zabbix_agentd/etc/zabbix_agent.conf
