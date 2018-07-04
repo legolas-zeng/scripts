48dcab35690da728c8b152f22d55edcabc5f730b
legolas-zeng <814382690@qq.com>
2018-07-04 11:48:14 +0800

    删除指定目录下某天前的文件、文件夹

---
+++ 随手记/del_file.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+import os,sys,time
+
+def removefiles(beftime, defalutpath='.'):
+    for i in os.listdir(path):
+        filename = path + os.sep + i
+        if os.path.getmtime(filename) < beftime:
+            try:
+                if os.path.isfile(filename):
+                    os.remove(filename)
+                elif os.path.isdir(filename):
+                    os.removedirs(filename)
+                else:
+                    os.remove(filename)
+                print "%s remove success." % filename
+            except Exception as error:
+                print error
+                print "%s remove faild." % filename
+
+
+if __name__ == "__main__":
+    try:
+        path = sys.argv[1]
+        breday = int(sys.argv[2])
+        bretime = time.time() - 3600 * 24 * breday
+        removefiles(bretime, path)
+    except Exception as e:
+        print e
+        sys.exit(-1)
