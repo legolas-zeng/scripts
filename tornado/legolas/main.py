e7dd2e3d09a7a50c0263460f87e58e6c76a0730f
legolas-zeng <qq1005521>
2018-11-05 23:25:13 +0800

    11.05

--- tornado/legolas/main.py
+++ tornado/legolas/main.py
@@ -5,6 +5,8 @@ import tornado.ioloop
 import tornado.httpserver  # 新引入httpserver模块
 import tornado.options # 新导入的options模块
 from tornado.web import url, RequestHandler
+import tornado.websocket
+
 import config
 
 tornado.options.define("port", default=8081, type=int, help="run server on the given port.")  # 定义服务器监听端口选项
@@ -12,8 +14,8 @@ tornado.options.define("itcast", default=[], type=str, multiple=True, help="itca
 
 class IndexHandler(RequestHandler):
     def get(self):
-        self.write("Hello Itcast!")
-
+        # self.write("Hello Itcast!")
+        self.render('index.html')
 
 class ItcastHandler(RequestHandler):
     def initialize(self, subject):
@@ -22,16 +24,40 @@ class ItcastHandler(RequestHandler):
     def get(self):
         self.write(self.subject)
 
+class WebsockertHandler(RequestHandler):
+    def check_origin(self, origin):
+        return True
+
+    def open(self):
+        pass
+
+    def on_message(self, message):
+        self.write_message(u"Your message was: " + message)
+
+    def on_close(self):
+        pass
+
+
+class Application(tornado.web.Application):
+    def __init__(self):
+        handlers = [
+            (r'/', IndexHandler),
+            (r'/ws', WebsockertHandler),
+            (r"/cpp", ItcastHandler, {"subject": "c++"}),
+            url(r"/python", ItcastHandler, {"subject": "python"}, name="python_url"),
+        ]
+        settings = {
+            'template_path': 'templates',
+            'static_path': 'static',
+            'static_url_prefix': '/static/',
+        }
+        tornado.web.Application.__init__(self, handlers, **settings,debug=True)
+
 
 if __name__ == "__main__":
     # tornado.options.parse_config_file("./config")  # 导入配置的路径
     tornado.options.parse_command_line()
-
-    app = tornado.web.Application([
-        (r"/", IndexHandler),
-        (r"/cpp", ItcastHandler, {"subject": "c++"}),
-        url(r"/python", ItcastHandler, {"subject": "python"}, name="python_url"),
-    ], debug=True)
+    app = Application()
 
     tornado.options.parse_command_line()
     print(tornado.options.options.itcast)  # 输出多值选项
