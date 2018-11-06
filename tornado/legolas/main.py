b884fe19fc35404378e6d295531285051aaf034f
legolas-zeng <814382690@qq.com>
2018-11-06 19:06:14 +0800

    websocket简单样式，调试通了

--- tornado/legolas/main.py
+++ tornado/legolas/main.py
@@ -1,18 +1,39 @@
 # coding:utf-8
-import tornado.web
-import tornado.ioloop
-import tornado.httpserver  # 新引入httpserver模块
-import tornado.options # 新导入的options模块
-from tornado.web import url, RequestHandler
-import tornado.websocket
+import tornado
+from tornado import web,websocket,httpserver,ioloop,options
+from tornado.web import url, RequestHandler,Application
 
-tornado.options.define("port", default=8081, type=int, help="run server on the given port.")  # 定义服务器监听端口选项
-tornado.options.define("itcast", default=[], type=str, multiple=True, help="itcast subjects.") # 无意义，演示多值情况
+options.define("port", default=8081, type=int, help="run server on the given port.")  # 定义服务器监听端口选项
+options.define("itcast", default=[], type=str, multiple=True, help="itcast subjects.") # 无意义，演示多值情况
 
-class IndexHandler(RequestHandler):
+class WebSocketHandler(tornado.websocket.WebSocketHandler):
+    def check_origin(self, origin):
+        return True
+
+    '''
+    新的WebSocket连接打开时被调用。
+    '''
+    def open(self):
+        pass
+
+    '''
+    连接收到新消息时被调用。
+    '''
+    def on_message(self, message):
+        self.write_message(u"Your message was: " + message)
+        print(message)
+        self.write_message(u"hahhahahah")
+    '''
+    在客户端关闭时被调用。
+    '''
+    def on_close(self):
+        pass
+
+
+class IndexPageHandler(RequestHandler):
     def get(self):
-        # self.write("Hello Itcast!")
-        self.render('index.html')
+        self.render("index.html")
+
 
 class ItcastHandler(RequestHandler):
     def initialize(self, subject):
@@ -21,52 +42,28 @@ class ItcastHandler(RequestHandler):
     def get(self):
         self.write(self.subject)
 
-class WebsockertHandler(RequestHandler):
-    def check_origin(self, origin):
-        return True
-    '''
-    新的WebSocket连接打开时被调用。
-    '''
-    def open(self):
-        self.application.shoppingCart.register(self.callback)
-    '''
-    连接收到新消息时被调用。
-    '''
-    def on_message(self, message):
-        self.write_message(u"Your message was: " + message)
-    '''
-    在客户端关闭时被调用。
-    '''
-    def on_close(self):
-        self.application.shoppingCart.unregister(self.callback)
 
-
-class Application(tornado.web.Application):
+class Application(Application):
     def __init__(self):
         handlers = [
-            (r'/', IndexHandler),
-            (r'/ws', WebsockertHandler),
+            (r'/', IndexPageHandler),
+            (r'/ws', WebSocketHandler),
             (r"/cpp", ItcastHandler, {"subject": "c++"}),
             url(r"/python", ItcastHandler, {"subject": "python"}, name="python_url"),
         ]
+
         settings = {
             'template_path': 'templates',
             'static_path': 'static',
             'static_url_prefix': '/static/',
         }
-        tornado.web.Application.__init__(self, handlers, **settings,debug=True)
+        tornado.web.Application.__init__(self, handlers, **settings)
 
 
-if __name__ == "__main__":
-    # tornado.options.parse_config_file("./config")  # 导入配置的路径
+if __name__ == '__main__':
     tornado.options.parse_command_line()
-    app = Application()
-
-    tornado.options.parse_command_line()
-    print(tornado.options.options.itcast)  # 输出多值选项
-
-    http_server = tornado.httpserver.HTTPServer(app)
+    ws_app = Application()
+    http_server = tornado.httpserver.HTTPServer(ws_app)
+    # server.listen(8081)
     http_server.listen(tornado.options.options.port)
-    # http_server.bind(8081)
-    # http_server.start(2)   # os.fork  不要在windows系统上用,不然会报错
-    tornado.ioloop.IOLoop.current().start()
+    tornado.ioloop.IOLoop.instance().start()
