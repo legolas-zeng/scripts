9e1fad5f690e1a1d7d22c781f38f631cf4261d12
legolas-zeng <814382690@qq.com>
2018-11-29 09:15:21 +0800

    代码归档

--- 随手记/5.py
+++ 随手记/5.py
@@ -1,7 +1,13 @@
 # coding=utf-8
 a = [1,2,3,4]
+
+print dir(a)
+for index,name in enumerate(a):
+    print index,'-',name
+
+
 for item in a:
     print item
 
-for i in range(len(a)):
+for i in range(len(a)): #直接迭代序列元素比迭代元素索引要快。
     print a[i]
