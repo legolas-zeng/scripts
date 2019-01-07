3142bbe8f32d626d3b680078bdd09f804f683565
legolas-zeng <814382690@qq.com>
2019-01-07 17:46:17 +0800

    注释

--- 协程/test.py
+++ 协程/test.py
@@ -1,6 +1,7 @@
 # coding=utf-8
 import requests
 import time,asyncio
+
 async def test2(i):
     r = await other_test(i)
     print(i,r)
@@ -12,14 +13,14 @@ async def other_test(i):
     print(time.time()-start)
     return r
 
-url = ["https://segmentfault.com/p/1210000013564725",
-       "https://www.jianshu.com/p/83badc8028bd",
+url = ["https://segmentfault.com",
+       "https://www.jianshu.com",
        "https://www.baidu.com/"]
 
-loop = asyncio.get_event_loop()
+loop = asyncio.get_event_loop() # 创建一个循环
 task = [asyncio.ensure_future(test2(i)) for i in url]
 start = time.time()
-loop.run_until_complete(asyncio.wait(task))
+loop.run_until_complete(asyncio.wait(task))  # 检测task运行情况并返回结果
 endtime = time.time()-start
 print(endtime)
 loop.close()
