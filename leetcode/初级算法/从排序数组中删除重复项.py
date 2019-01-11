dcc96ef0af0ac18fa22e1395f9f49399e83c2336
legolas-zeng <814382690@qq.com>
2019-01-11 19:39:13 +0800

    算法归档

---
+++ leetcode/初级算法/从排序数组中删除重复项.py
@@ -0,0 +1,14 @@
+# coding=utf-8
+
+'''
+给定一个排序数组，你需要在原地删除重复出现的元素，使得每个元素只出现一次，返回移除后数组的新长度。
+不要使用额外的数组空间，你必须在原地修改输入数组并在使用 O(1) 额外空间的条件下完成。
+'''
+
+nums = [1,1,2]
+new_nums = []
+for x in nums:
+    if x not in new_nums:
+        new_nums.append(x)
+
+print(new_nums)
