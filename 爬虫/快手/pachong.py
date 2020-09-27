b94386cc322be4b808e35ddba15c50f4abc96b7d
legolas-zeng <814382690@qq.com>
2020-09-27 17:55:39 +0800

    测试

---
+++ 爬虫/快手/pachong.py
@@ -0,0 +1,15 @@
+# coding=utf-8
+import re
+import requests
+
+from urllib import parse
+from bs4 import BeautifulSoup
+
+# def main():
+#     res = requests.post("https://www.apibug.com/api/dsp/kuaishou.php?url=https://v.kuaishou.com/6FXp0H")
+#     print(res.text)
+#
+# if __name__ == '__main__':
+#     main()
+
+
